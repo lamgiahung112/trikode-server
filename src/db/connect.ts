0e041342eaccc48ac24fb93eb69fa172809d4b62
Hung PC <lamgiahung112@gmail.com>
2024-03-26 15:29:41 +0700

    add connection pool

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -1,7 +1,12 @@
 import mongoose from "mongoose"
 
 export default async function connectDB() {
-	mongoose.connect("mongodb://127.0.0.1:27017/trikode").then(() => {
-		console.log("CONNECTED TO DB")
-	})
+	mongoose
+		.connect("mongodb://127.0.0.1:27017/trikode", {
+			minPoolSize: 5,
+			maxPoolSize: 10,
+		})
+		.then(() => {
+			console.log("CONNECTED TO DB")
+		})
 }
